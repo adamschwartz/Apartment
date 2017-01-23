7f70a2893f1fe802fcf30eeca5b1b9906687c5e8
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2017-01-23 01:50:05 -0500

    fix bug with xyBriToHex

--- index.ios.js
+++ index.ios.js
@@ -45,7 +45,7 @@ var xyBriToHex = function(x, y, bri) {
   b = Math.round(b).toString(16)
   if (r.length < 2) { r = '0' + r }
   if (g.length < 2) { g = '0' + g }
-  if (b.length < 2) { b = '0' + r }
+  if (b.length < 2) { b = '0' + b }
   return '#' + r + g + b
 }
 
